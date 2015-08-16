0cbf4eb13084dac48929da64ce0b393bbab17017
Andres Felipe Garcia Duran <andresduran0502@gmail.com>
2015-08-16 15:47:38 -0500

    Estructura grafica /Administracion usuario

--- WebAppAdministracionEscolar/src/java/com/lusadi/beans/AdminUsuariosBean.java
+++ WebAppAdministracionEscolar/src/java/com/lusadi/beans/AdminUsuariosBean.java
@@ -23,61 +23,6 @@ import org.primefaces.model.TreeNode;
 @ViewScoped
 public class AdminUsuariosBean implements Serializable {
 
-    private TreeNode root;
-    private TreeNode selectedNode;
-
-    public AdminUsuariosBean() {
-    }
-
-    @PostConstruct
-    public void init() {
-        root = createDocuments();
-    }
-
-    public TreeNode getRoot() {
-        return root;
-    }
-
-    public TreeNode getSelectedNode() {
-        return selectedNode;
-    }
-
-    public void setSelectedNode(TreeNode selectedNode) {
-        this.selectedNode = selectedNode;
-    }
-
-    public void displaySelectedSingle() {
-        if (selectedNode != null) {
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected", selectedNode.getData().toString());
-            FacesContext.getCurrentInstance().addMessage(null, message);
-        }
-    }
-
-    public void deleteNode() {
-        selectedNode.getChildren().clear();
-        selectedNode.getParent().getChildren().remove(selectedNode);
-        selectedNode.setParent(null);
-
-        selectedNode = null;
-    }
-
-    private TreeNode createDocuments() {
-        TreeNode root = new DefaultTreeNode(new Funcion("Funciones", null, null), null);
-
-        TreeNode node01 = new DefaultTreeNode(new Funcion("Docente", null, null), root);
-        node01.setExpanded(true);
-        TreeNode node02 = new DefaultTreeNode(new Funcion("Alumno", null, null), root);
-        node02.setExpanded(true);
-
-        TreeNode node01_01 = new DefaultTreeNode(new Funcion("Registrar Docente", "registro-docente.xhtml", "D"), node01);
-        TreeNode node01_02 = new DefaultTreeNode(new Funcion("Busqueda Docente", "busqueda-docente.xhtml", "D"), node01);
-        TreeNode node01_03 = new DefaultTreeNode(new Funcion("Eliminar Docente", "busqueda-docente.xhtml", "D"), node01);
-
-        TreeNode node02_01 = new DefaultTreeNode(new Funcion("Registrar Alumno", "registro-docente.xhtml", "D"), node02);
-        TreeNode node02_02 = new DefaultTreeNode(new Funcion("Busqueda Alumno", "busqueda-docente.xhtml", "D"), node02);
-        TreeNode node02_03 = new DefaultTreeNode(new Funcion("Eliminar Alumno", "busqueda-docente.xhtml", "D"), node02);
-
-        return root;
-    }
+  
 
 }
---
+++ WebAppAdministracionEscolar/src/java/com/lusadi/beans/HomeConfigBean.java
@@ -0,0 +1,96 @@
+package com.lusadi.beans;
+
+import com.lusadi.modelo.Funcion;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.NodeSelectEvent;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
+
+/**
+ *
+ * @author andresfelipegarciaduran
+ */
+@Named(value = "homeConfigBean")
+@ViewScoped
+public class HomeConfigBean implements Serializable {
+
+    private TreeNode root;
+    private TreeNode selectedNode;
+    private String pathForwardMenu01 = "/modulos/admin-usuario/menu-administrador.xhtml";
+
+    public HomeConfigBean() {
+    }
+
+    @PostConstruct
+    public void init() {
+        root = createDocuments();
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public TreeNode getSelectedNode() {
+        return selectedNode;
+    }
+
+    public void setSelectedNode(TreeNode selectedNode) {
+        this.selectedNode = selectedNode;
+    }
+
+    public void displaySelectedSingle() {
+        if (selectedNode != null) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected", selectedNode.getData().toString());
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+
+    public void deleteNode() {
+        selectedNode.getChildren().clear();
+        selectedNode.getParent().getChildren().remove(selectedNode);
+        selectedNode.setParent(null);
+
+        selectedNode = null;
+    }
+
+    private TreeNode createDocuments() {
+        TreeNode root = new DefaultTreeNode(new Funcion("Funciones", null, null), null);
+
+        TreeNode node01 = new DefaultTreeNode(new Funcion("Docente", null, null), root);
+        node01.setExpanded(true);
+        TreeNode node02 = new DefaultTreeNode(new Funcion("Alumno", null, null), root);
+        node02.setExpanded(true);
+
+        TreeNode node01_01 = new DefaultTreeNode(new Funcion("Registrar Docente", "registro-docentes.xhtml", "D"), node01);
+        TreeNode node01_02 = new DefaultTreeNode(new Funcion("Busqueda Docente", "busqueda-docente.xhtml", "D"), node01);
+        TreeNode node01_03 = new DefaultTreeNode(new Funcion("Eliminar Docente", "busqueda-docente.xhtml", "D"), node01);
+
+        TreeNode node02_01 = new DefaultTreeNode(new Funcion("Registrar Alumno", "registro-docente.xhtml", "D"), node02);
+        TreeNode node02_02 = new DefaultTreeNode(new Funcion("Busqueda Alumno", "busqueda-docente.xhtml", "D"), node02);
+        TreeNode node02_03 = new DefaultTreeNode(new Funcion("Eliminar Alumno", "busqueda-docente.xhtml", "D"), node02);
+
+        return root;
+    }
+
+    public void onFunctionSelectMenu01(NodeSelectEvent event) {
+        Funcion funcion = (Funcion) event.getTreeNode().getData();
+        System.out.println(funcion.getNombreFuncion());
+        setPathForwardMenu01("/modulos/admin-usuario/" + funcion.getUrlFuncion());
+        RequestContext.getCurrentInstance().update("center");
+    }
+
+    public String getPathForwardMenu01() {
+        return pathForwardMenu01;
+    }
+
+    public void setPathForwardMenu01(String pathForwardMenu01) {
+        this.pathForwardMenu01 = pathForwardMenu01;
+    }
+
+}
