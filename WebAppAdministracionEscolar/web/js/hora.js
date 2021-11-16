61bdbc62b69b523b17b3a7dcc0c546ef992c5cd0
Sebastian Vega <jzebas94@gmail.com>
2021-11-15 19:50:28 -0500

    Delete WebAppAdministracionEscolar directory

--- WebAppAdministracionEscolar/web/js/hora.js
+++
@@ -1,6 +0,0 @@
-function horaActual() {
-    var meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-    var f = new Date();
-    document.write(f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
-}
-
